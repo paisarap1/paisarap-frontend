7984772f16e0113cb11b1f1b3f7c2c71d0766ab1
ady <nnf5629@gmail.com>
2025-08-02 14:52:33 +0530

    Fixed missing react-router-dom

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login Page - Paisarap</h1>
+    </div>
+  );
+}
---
+++ src/pages/Register.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Register() {
+  return (
+    <div>
+      <h1>Register Page - Paisarap</h1>
+    </div>
+  );
+}
